6fc17e4e9e4b5a8b876b09c71eaaa676e596a967
dev <sempernow.com@gmail.com>
2025-06-30 19:41:25 -0400

    make.sh @ 2025-08-01T23:06Z

---
+++ README.md
@@ -0,0 +1,4 @@
+# [`sempernow/oci-patch`](https://github.com/sempernow/oci-patch "GitHub")
+
+Patch OCI container images
+
---
+++ make.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash 
+[[ $3 ]] || {
+    echo "  USAGE: ${BASH_SOURCE##*/} "'build|inspect|push|run '"APP VER COMMIT" >&2 
+    echo "         ${BASH_SOURCE##*/} scan REGISTRY/REPO/IMAGE TAG" >&2 
+
+    exit 11
+}
+repo=gd9h
+app=$2
+ver=$3
+commit=$4
+date=$(stat --format=%y trivy*$app*.log 2>/dev/null |cut -d' ' -f1 |head -n1)
+[[ $date ]] || date=$(date -Id)
+tag="${ver}-patch-${date//-/}"
+
+scan(){
+    img="$1:$2"
+    fname=${img////.}
+    trivy image --scanners vuln --severity 'CRITICAL,HIGH' $img \
+        |tee trivy.${fname//:/_}.log
+}
+build(){
+    [[ -f $app.oci ]] || return 22
+    docker build \
+        --build-arg force_rebuild="$(date +%s)" \
+        --build-arg VERSION="$ver" \
+        --build-arg TAG="$tag" \
+        --build-arg DATE="$(date -Id)" \
+        --build-arg IMAGE_TARGET="$repo/$app:$tag" \
+        --build-arg COMMIT="$commit" \
+        -t $repo/$app:$tag \
+        -f $app.oci .
+
+    scan $repo/$app $tag
+}
+inspect(){
+    type -t yq >/dev/null 2>&1 && {
+        docker image inspect $repo/$app:$tag \
+            |yq eval -P -o yaml \
+            |yq '.[] | (.RepoTags,.RepoDigests,.Config)'
+
+        return $?  
+    }
+    type -t jq >/dev/null 2>&1 && {
+        docker image inspect $repo/$app:$tag |jq '.[] | {
+            "Tags": .RepoTags,
+            "Digests": .RepoDigests,
+            "Config": .Config
+        }'
+        return $?
+    }
+    docker image inspect "$repo/$app:$tag"
+}
+push(){
+    docker push $repo/$app:$tag || return 33
+    docker tag $repo/$app:$tag $repo/$app:latest
+    docker push $repo/$app:latest || return 34
+}
+run(){
+    shift;shift
+    docker run -it --rm --name $app $repo/$app:$tag "$@" 
+}
+
+"$@" || echo "❌  ERR : $?" >&2
