fb8ef02dbf2f44a13b7dda6ce237931b2f509368
dev <sempernow.com@gmail.com>
2025-07-11 21:25:15 -0400

    .gitignore @ 2025-07-12T01:24Z

--- iperf3/trivy.image.sh
+++
@@ -1,1 +0,0 @@
-trivy image --scanners=vuln --severity='CRITICAL,HIGH' gd9h/iperf:3.19-hard |tee trivy.image.gd9h.iperf_3.19-hard.log
--- make.sh
+++ make.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash 
-## Gitlab Product Documentation : OCI Container Image 
-## The container runs a static website served by NGINX on Alpine.
-## Images: https://gitlab.com/gitlab-org/technical-writing/docs-gitlab-com/container_registry/8244403
-## How to: https://docs.gitlab.com/administration/docs_self_host/#self-host-the-product-documentation-with-docker
+[[ $3 ]] || {
+    echo "⚠️  Missing required ARGs" >&2 
 
-app='gitlab-docs'
-ver='18.1'
+    exit 11
+}
+app=$2
+ver=$3
 repo=gd9h/$app
-date=$(stat --format=%y trivy*-patch-*.log 2>/dev/null |cut -d' ' -f1)
+date=$(stat --format=%y trivy*$tag*.log 2>/dev/null |cut -d' ' -f1 |head -n1)
 [[ $date ]] || date=$(date -Id)
 tag="${ver}-patch-${date//-/}"
 
 build(){
+    [[ -f $app.oci ]] || return 22
     docker build \
         --build-arg FORCE_REBUILD="$(date +%s)" \
         --build-arg VERSION="$ver" \
@@ -35,10 +36,10 @@ scan(){
         |tee trivy.${repo////.}_$tag.log
 }
 push(){
-    docker push $repo:$tag
+    docker push $repo:$tag || return 33
 }
 run(){
-    docker run -it --rm --name $app $repo:$tag "$@" 
+    docker run -it --rm --name $app $repo:$tag "$3" 
 }
 
-"$@" || echo "  ERR : $?"
+"$@" || echo "❌  ERR : $?" >&2
